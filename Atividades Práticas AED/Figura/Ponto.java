2f0d8117dbd66aec5aa849ef0e8a66bc847ca2f8
Gabriel Uzel <84882077+ErgoMaster@users.noreply.github.com>
2021-12-17 13:35:00 -0300

    Add files via upload

---
+++ Atividades Práticas AED/Figura/Ponto.java
@@ -0,0 +1,22 @@
+public class Ponto {
+    private int x, y;
+
+    public Ponto() {
+        x = 0;
+        y = 0;
+    }
+    public Ponto(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Métodos get
+    public int getX() { return x; }
+    public int getY() { return y; }
+
+    // Métodos set
+    public void setX(int x) { this.x = x; }
+    public void setY(int y) { this.y = y; }
+
+    public String toString() { return "[" + getX() + "," + getY() + "]"; }
+}
