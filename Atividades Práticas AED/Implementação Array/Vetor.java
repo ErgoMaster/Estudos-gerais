448edf5346b249a77cd3d91f76439056d2948881
ErgoMaster <gabriel.uzel1@gmail.com>
2022-03-11 12:27:16 -0300

    Update

--- Atividades Práticas AED/Implementação Array/Vetor.java
+++ Atividades Práticas AED/Implementação Array/Vetor.java
@@ -9,12 +9,10 @@ public class Vetor {
     public Vetor() { this(0,0); }
     public Vetor(int tamanho) { this(tamanho, 0); }
 
-    // Métodos get:
     public Object getData() { return data; }
     public int getBase() { return base; }
     public int getlength() { return data.length; }
 
-    // Método para atribuir os valores de um array "a" para um array qualquer:
     public void assing(Vetor a) {
         if(a != this) {
             if(data.length != a.data.length) {
@@ -29,12 +27,9 @@ public class Vetor {
         }
     }
 
-    // Método para o retorno de um certo elemento dada sua posição no array:
     public Object getElemento(int index) { return data[index - base]; }
-    // Método para mudança do valor de um elemento dada sua posição no array:
     public void setElemento(int index, Object object) { data[index - base] = object; }
 
-    // Métodos para redimensionar o tamanho ou base do array:
     public void setBase(int newBase) { base = newBase; }
     public void setLength(int newLen) {
         if(data.length != newLen) {
--- Atividades Práticas AED/Implementação Deque/DequeEncadeado.java
+++ Atividades Práticas AED/Implementação Deque/DequeEncadeado.java
@@ -1,26 +1,58 @@
 public class DequeEncadeado implements DequeInterface {
-    private ListaEncadeada lista;
+    
 
     @Override
-    public Object getUltimo() { }
-    
+    public void fazVazia() {
+        
+    }
+
+    @Override
+    public boolean estaVazia() {
+        return false;
+    }
+
+    @Override
+    public Object getPrimeiro() {
+        return null;
+    }
+
+    @Override
+    public void enfileirar(Object item) {
+        
+    }
+
+    @Override
+    public Object desenfileirar() throws CountVazioException {
+        return null;
+    }
+
+    @Override
+    public Object getUltimo() {
+        return null;
+    }
+
     @Override
     public void enfileirarInicio(Object item) {
-
+        
     }
 
     @Override
     public void enfileirarFim(Object item) {
-
+        
     }
 
     @Override
     public Object desenfileirarInicio() {
-
+        return null;
     }
 
     @Override
     public Object desenfileirarFim() {
+        return null;
+    }
 
-    } 
+    @Override
+    public String toString() {
+        return "";
+    }
 }
