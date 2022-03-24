7bf76a9a65cdbfe364eb909fed12b54e24a53db4
ErgoMaster <gabriel.uzel1@gmail.com>
2022-03-24 14:41:55 -0300

    Update

--- Implementação de TAD's/InfixTOPostfix/Main.java
+++ Implementação de TAD's/InfixTOPostfix/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        InfixToPostfix programa = new InfixToPostfix("5+((4*3)/2)");
+        InfixToPostfix programa = new InfixToPostfix("4+2/(5*7)");
 
         System.out.println(programa);
     }
